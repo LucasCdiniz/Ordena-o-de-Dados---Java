76c6a771027cb2084f29915ebe2c51646c1b39d3
Lucas Diniz <49011583+LucasCdiniz@users.noreply.github.com>
2019-06-11 22:23:29 -0300

    Add files via upload

---
+++ Contem_as_Ordenacoes_de_Dados.java
@@ -0,0 +1,30 @@
+// Criado e Desenvolivdo por:
+// Lucas Celestino Diniz
+// lucascelestino.diniz@gmail.com
+
+/*
+    Programa que realiza a Ordenação de Dados utilizandos as ordenações:
+    BubbleSort;SelectionSort;InsertionSort;QuickSort;MergeSort
+    Com a possibilidade de informar dez (10) números para realizar a Ordenação que desejar
+    Assim como há a possibilidade de gerar estes números aleatóriamente
+    Tratamento de erro realizado para que o usuário digite apenas números e nenhuma letra
+    Tratamento de erro realizado também para que o usuário não prossiga ao deixar a opção em branco
+    Tratamento de erro para que o cliente digite apenas os números que estão dispostos no formulário
+*/
+
+
+
+package ordenacaodedados;
+
+// INÍCIO DA CLASSE CONTEM_AS ORDENACAO_DE_DADOS E A HERENÇA DA CLASSE EXECUTA_AS ORDENACAO_DE_DADOS
+public class Contem_as_Ordenacoes_de_Dados extends Executa_a_Ordenacao_de_Dados { 
+        
+        public void Executa_Ordenacao(){
+            super.Executa_a_Ordenacao();
+        }
+}
+
+
+// Criado e Desenvolivdo por:
+// Lucas Celestino Diniz
+// lucascelestino.diniz@gmail.com
